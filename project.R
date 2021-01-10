f8958c2f146e2a8ec99e770ca49a123cb2f49d15
andretavluis <74914865+andretavluis@users.noreply.github.com>
2021-01-10 12:49:27 +0000

    Update project.R
    pca + clustering

--- project.R
+++ project.R
@@ -92,7 +92,199 @@ dev.off()
 
 # transform data (only 3 PC's so consider the whole set)
 data_afterROBPCA<-getScores(results_pcaRobust)
+
 #----------------------------------------------------------------
+#-----------Clustering-------------------------------------------
+#----------------------------------------------------------------
+library(dplyr)
+library(cluster)
+library(purrr)
+library(ClusterR)
+library(tidyverse)
+
+outcome <- read.csv("WDI_shortnames.csv", header = TRUE, sep=",")
+outcome = outcome[,72]
+#-------MRMR dataset------------------------------------------------------------------------
+#standardize data
+data=scale(data, center = TRUE, scale = TRUE)
+
+#------------ kmeans---------- 
+#SELECT K
+#Elbow method - based on the plot, select k=3 (elbow)
+wssplot <- function(data, nc=10, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$tot.withinss)}
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Total within cluster sum of squares")}
+
+wssplot(data)
+
+#Silhouette method
+# function to compute average silhouette for k clusters
+avg_sil <- function(k) {
+  km.res <- kmeans(data, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(data))
+  mean(ss[, 3])
+}
+# Compute and plot wss for k = 2 to k = 15
+k.values <- 2:10
+# extract avg silhouette for 2-15 clusters
+avg_sil_values <- map_dbl(k.values, avg_sil)
+
+plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+#Choose k=2 or k=3
+k2 = kmeans(data, 2, nstart=20)
+k2$betweenss/k2$totss
+k2
+clusplot(data, k2$cluster, color=TRUE, shade=TRUE, labels=4, lines=0, main = 'MRMR Kmeans, k=2')
+
+k3 = kmeans(data, 3, nstart=20)
+k3$betweenss/k3$totss
+k3
+clusplot(data, k3$cluster, color=TRUE, labels=4, lines=0, main = 'MRMR Kmeans, k=3', col.p = outcome)
+legend("topright", inset=.02, title="HDI class",
+       c("Negative", "Low", "Medium", "High"), fill=c("yellow", "black", "red", "green"), cex=0.7)
+
+#write to csv
+clust_names <- k3$cluster
+data <- cbind(data, clust_names)
+write.csv(data, 'WDI_CLUSTERING.csv')
+
+
+#-------------kmedoids-----------
+k2 = Cluster_Medoids(data,clusters=2,distance_metric="manhattan")
+k2
+clusplot(data, k2$clusters, color=TRUE, shade=TRUE, labels=4, lines=0, main = 'MRMR Kmedoids, k=2')
+
+k3 = Cluster_Medoids(data, clusters=3,distance_metric="manhattan")
+k3
+clusplot(data, k3$clusters, color=TRUE, labels=4, lines=0,
+         main = 'MRMR Kmedoids, k=3', col.p = outcome)
+legend("topright", inset=.02, title="HDI class",
+       c("Negative", "Low", "Medium", "High"), fill=c("yellow", "black", "red", "green"), cex=0.7)
+
+
+#------------Classical PCA----------------------------------------------------------------
+data_afterPCA
+
+#------------ kmeans---------- 
+#SELECT K
+#Elbow method - based on the plot, select k=3 (elbow)
+wssplot <- function(data, nc=10, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$tot.withinss)}
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Total within cluster sum of squares")}
+
+wssplot(data_afterPCA)
+
+#Silhouette method
+# function to compute average silhouette for k clusters
+avg_sil <- function(k) {
+  km.res <- kmeans(data_afterPCA, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(data_afterPCA))
+  mean(ss[, 3])
+}
+# Compute and plot wss for k = 2 to k = 15
+k.values <- 2:10
+# extract avg silhouette for 2-15 clusters
+avg_sil_values <- map_dbl(k.values, avg_sil)
+
+plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+#Choose k=2 or k=3
+k2 = kmeans(data_afterPCA, 2, nstart=20)
+k2$tot.withinss
+k2
+clusplot(data_afterPCA, k2$cluster, color=TRUE, shade=TRUE, labels=4, lines=0, main = 'PCA Kmeans, k=2')
+
+k3 = kmeans(data_afterPCA, 3, nstart=20)
+k3$tot.withinss
+k3
+clusplot(data_afterPCA, k3$cluster, color=TRUE, labels=4, lines=0, main = 'PCA Kmeans, k=3', col.p = outcome)
+legend("topleft", inset=.02, title="HDI class",
+       c("Negative", "Low", "Medium", "High"), fill=c("yellow", "black", "red", "green"), cex=0.7)
+
+#-------------kmedoids-----------
+k2 = Cluster_Medoids(data_afterPCA, clusters=2,distance_metric="manhattan")
+k2
+clusplot(data_afterPCA, k2$clusters, color=TRUE, shade=TRUE, labels=4, lines=0, main = 'PCA Kmedoids, k=2')
+
+k3 = Cluster_Medoids(data_afterPCA, clusters=3,distance_metric="manhattan")
+k3
+clusplot(data_afterPCA, k3$clusters, color=TRUE, labels=4, lines=0, main = 'PCA Kmedoids, k=3', col.p = outcome)
+legend("topleft", inset=.02, title="HDI class",
+       c("Negative", "Low", "Medium", "High"), fill=c("yellow", "black", "red", "green"), cex=0.7)
+
+#------------Robust PCA---------------------------------------------
+#perform clustering only on the 2 first robust principal components
+data_afterROBPCA <- data_afterROBPCA[,-3]
+
+#------------ kmeans---------- 
+#SELECT K
+#Elbow method - based on the plot, select k=3 (elbow)
+wssplot <- function(data, nc=10, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$tot.withinss)}
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Total within cluster sum of squares")}
+
+wssplot(data_afterROBPCA)
+
+#Silhouette method
+# function to compute average silhouette for k clusters
+avg_sil <- function(k) {
+  km.res <- kmeans(data_afterROBPCA, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(data_afterROBPCA))
+  mean(ss[, 3])
+}
+# Compute and plot wss for k = 2 to k = 15
+k.values <- 2:10
+# extract avg silhouette for 2-15 clusters
+avg_sil_values <- map_dbl(k.values, avg_sil)
+
+plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+#Choose k=2 or k=3
+k2 = kmeans(data_afterROBPCA, 2, nstart=20)
+k2$tot.withinss
+k2
+clusplot(data_afterROBPCA, k2$cluster, color=TRUE, shade=TRUE, labels=4, lines=0, main = 'ROBPCA Kmeans, k=2')
+
+k3 = kmeans(data_afterROBPCA, 3, nstart=20)
+k3$tot.withinss
+k3
+clusplot(data_afterROBPCA, k3$cluster, color=TRUE, labels=4, lines=0, main = 'ROBPCA Kmeans, k=3', col.p = outcome)
+legend("bottomright", inset=.02, title="HDI class",
+       c("Negative", "Low", "Medium", "High"), fill=c("yellow", "black", "red", "green"), cex=0.6)
+
+#-------------kmedoids-----------
+k2 = Cluster_Medoids(data_afterROBPCA, clusters=2,distance_metric="manhattan")
+k2
+clusplot(data_afterROBPCA, k2$clusters, color=TRUE, shade=TRUE, labels=4, lines=0, main = 'ROBPCA Kmedoids, k=2')
+
+k3 = Cluster_Medoids(data_afterROBPCA, clusters=3,distance_metric="manhattan")
+k3
+clusplot(data_afterROBPCA, k3$clusters, color=TRUE, labels=4, lines=0, main = 'ROBPCA Kmedoids, k=3', col.p = outcome)
+legend("bottomright", inset=.02, title="HDI class",
+       c("Negative", "Low", "Medium", "High"), fill=c("yellow", "black", "red", "green"), cex=0.6)
+
 
 
 
